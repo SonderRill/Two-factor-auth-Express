1c00ed035143fb4a82fa4bf6cc61309ed882e07d
Kirill <49909245+SonderRill@users.noreply.github.com>
2020-01-19 14:46:12 +0600

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({extended : true}))
 app.use(express.json())
 
 app.use('/auth', require('./routes/auth'));
-app.use('/routes', require('./routes/twofactot'));
+app.use('/routes', require('./routes/twofactor'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server has been started ${PORT}`))
